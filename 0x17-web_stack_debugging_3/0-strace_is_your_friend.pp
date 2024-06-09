ba99ede6564ed9936b44c9b62d498479310591dd
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-06-09 00:56:30 -0700

    fixed lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,12 +3,12 @@
 exec { 'Fix wordpress site':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
-  notify => Service['apache2'],
+  notify   => Service['apache2'],
 }
 
 service { 'apache2':
-  ensure => 'running',
-  enable => true,
+  ensure    => 'running',
+  enable    => true,
   subscribe => Exec['Fix wordpress site'],
 }
 
